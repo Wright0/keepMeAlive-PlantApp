dc4e3ea69e8b7f79cd1d62301f361c867447d419
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2020-03-11 15:05:17 +0000

    fixed issue with array inside function checking for localStorage

--- client/src/containers/HomeContainer.js
+++ client/src/containers/HomeContainer.js
@@ -10,23 +10,24 @@ class HomeContainer  extends Component  {
   // localStorage.setItem('playerId', 1)
 
   checkPlayerIdIsInLocalStorage = () => {
-    this.props.players.forEach(player => {
-      if (player["id"] !== parseInt(localStorage.getItem('playerId'))) {
-        // newUser = <NewUser />
-        return <h1> NO ID</h1>
-      }
-      else {
-        // return selectPlant
-        return <h1> ID</h1>
+    const array = []
+    this.props.players.find(player => {
+      if (player["id"] === parseInt(localStorage.getItem('playerId'))) {        
+        array.push(player)
       }
     })
+    if (array.length >= 1){      
+      return <SelectPlant plants={this.props.plants}/>
+    } else {
+      return <NewUser />
+    }
   }
 
   render(){
     return (
       <>
       <h1>HOME CONTAINER</h1>
-      {this.checkPlayerIdIsInLocalStorage}
+      {this.checkPlayerIdIsInLocalStorage()}
       </>
     )
   }
