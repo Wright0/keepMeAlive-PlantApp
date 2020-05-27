6b1c9d1e31759ee7ee65c750ea4968da2e16ec28
Olivia <olivia.wright101@gmail.com>
2020-05-27 11:20:37 +0100

    removed the server from this repo

--- server/src/main/java/com/codeclan/example/server/repositories/PlantRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codeclan.example.server.repositories;
-
-import com.codeclan.example.server.models.Plant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlantRepository extends JpaRepository<Plant, Long> {
-}
--- server/src/main/java/com/codeclan/example/server/repositories/PlayerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codeclan.example.server.repositories;
-
-import com.codeclan.example.server.models.Player;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-public interface PlayerRepository  extends JpaRepository<Player, Long> {
-}
